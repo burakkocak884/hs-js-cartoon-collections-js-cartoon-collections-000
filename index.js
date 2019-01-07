bb8ea9c2a9ca85116fe1bf1b8beaaee3cc78f408
BURAK KOCAK <burakkocak884@hotmail.com>
2019-01-07 18:14:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function longPlaneteerCalls(words) {
 function findTheCheese (foods) {
   cheeses =[ "gouda", "cheddar", "brie"]
     return foods[0]
+  
   foods.find
+  var found = foods.find
   
   
   
