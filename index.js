5975fce4e3ee068b2017732c2129cbcd50821974
BURAK KOCAK <burakkocak884@hotmail.com>
2019-01-07 18:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ function findTheCheese (foods) {
 foods = ["apple", "banana", "brie","gouda"]
 findTheCheese(foods)
 function wordsWithB(words){
-
-words.filter(function(word){
+  
+return words.filter(function(word){
   return word[0] == "b"
   
 } )
-return words
+
  
 }
 
